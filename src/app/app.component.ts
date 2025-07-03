221c55d035bb23497dc649bbeab93580d3ad7f08
Adam Camarillo <Adam.Camarillo@vixxo.com>
2025-07-03 14:44:24 -0700

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from '@progress/kendo-angular-buttons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [CommonModule, ButtonModule],
+  template: `
+  <h1>PCR Web Application - Angular 20</h1>
+  <button kendoButton themeColor="primary" size="large">
+    Kendo Primary Button
+  </button>
+  <br><br>
+  <button>Regular HTML Button</button>
+`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
